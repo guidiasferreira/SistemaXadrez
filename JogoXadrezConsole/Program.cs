9a71e00b1a4111bdbcba8d7ec025d71ab46822e7
Guilherme <gui.ferreira2606@gmail.com>
2025-07-10 22:19:19 -0300

    Criando a classe 'Posicao'

--- JogoXadrezConsole/Program.cs
+++ JogoXadrezConsole/Program.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace JogoXadrezConsole {
     internal class Program {
         static void Main(string[] args) {
+
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+
+            Console.ReadKey();
         }
     }
 }
